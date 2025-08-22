2a9fde1be607d51cc5b4a40fafdc2b1134b0fc2e
AndrewO123456789 <spreadbettingworld@gmail.com>
2025-08-22 16:49:01 +0100

    Update firebaseConfig.js

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -1,12 +1,12 @@
-// public/js/firebaseConfig.js
+// File: /public/js/firebaseConfig.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCtrZRTIKiN_6UasKzkemvEbRkSCMow6Qo",
   authDomain: "swing-trader-6431c.firebaseapp.com",
   projectId: "swing-trader-6431c",
-  storageBucket: "swing-trader-6431c.appspot.com", // ✅ corrected domain
+  storageBucket: "swing-trader-6431c.appspot.com",
   messagingSenderId: "255789637374",
   appId: "1:255789637374:web:1c12a2f513e98559e64faf"
 };
